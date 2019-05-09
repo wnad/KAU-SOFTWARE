f761d2d76836a42f47debd694e1683eb2f64cf08
wnad <rutc98@naver.com>
2019-05-09 19:59:06 +0900

    05.09.19.59

--- KAU_Java_LGH/Fig 14.15/src/ButtonTest.java
+++ KAU_Java_LGH/Fig 14.15/src/ButtonTest.java
@@ -1,9 +1,12 @@
+import javax.swing.JFrame;
 
 public class ButtonTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ButtonFrame buttonFrame = new ButtonFrame();
+		buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		buttonFrame.setSize(275,110);
+		buttonFrame.setVisible(true);
 	}
 
 }
